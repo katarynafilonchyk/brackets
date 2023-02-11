0ba03f5a1d67b066ecbe8ecb77fa6d6774a3bf96
Kataryna Filonchyk <Katarynafilonchyk@gmail.com>
2023-02-11 15:01:43 +0300

    inint: the end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const brackets_pair = new Map(bracketsConfig);
+  let stack = [];
+  for (const i of str) { 
+    if (brackets_pair.get(stack.at(-1)) === i) {
+      stack.pop(); 
+    } else { 
+      stack.push(i);
+    }
+  }
+  return stack.length === 0;
+  }
+ 
+  
